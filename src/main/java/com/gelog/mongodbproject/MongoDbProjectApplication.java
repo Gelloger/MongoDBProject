d80b2f9cb4655b1a0f388db9db3981eda96878b9
박세환/메세징플랫폼개발팀/NE <gehwan96@nhn.com>
2022-08-06 16:33:01 +0900

    Create MongoDB Project
    몽고디비 프로젝트 생성

---
+++ src/main/java/com/gelog/mongodbproject/MongoDbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gelog.mongodbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbProjectApplication.class, args);
+	}
+
+}
